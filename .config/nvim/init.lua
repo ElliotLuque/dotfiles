ab19d38cd61a1f90f5e0088fc2f591758e459ba3
ElliotLuque <elliotluque@gmail.com>
2024-07-10 14:22:11 +0200

    add togglewrap keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -162,6 +162,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Toggle line wrap
+vim.keymap.set('n', '<leader>ww', '<cmd>set wrap!<CR>')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
