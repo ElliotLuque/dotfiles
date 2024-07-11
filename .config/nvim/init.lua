767a87ed2fb59355e540b43f35be9a7230288106
ElliotLuque <elliotluque@gmail.com>
2024-07-11 19:15:36 +0200

    add glsl treesitter support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -792,11 +792,23 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'glsl' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = false },
+      }
+
+      -- Add filetype recognition for GLSL extensions
+      vim.filetype.add {
+        extension = {
+          vert = 'glsl',
+          frag = 'glsl',
+          geom = 'glsl',
+          tesc = 'glsl',
+          tese = 'glsl',
+          comp = 'glsl',
+        },
       }
 
       -- There are additional nvim-treesitter modules that you can use to interact
