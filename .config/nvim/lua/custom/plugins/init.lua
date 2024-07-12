acac883de374940b34e1f6ce0e16f328a64aad82
ElliotLuque <elliotluque@gmail.com>
2024-07-12 18:40:12 +0200

    add autopairs

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -71,4 +71,11 @@ return {
       require('nvim-ts-autotag').setup()
     end,
   },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
 }
